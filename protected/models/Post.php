2fb8978308f53af3c4c438a9ef99e8df91b72e40
karmeljuk <karmeljuk@gmail.com>
2014-12-17 21:37:11 +0200

    post model

--- protected/models/Post.php
+++ protected/models/Post.php
@@ -19,6 +19,10 @@
  */
 class Post extends CActiveRecord
 {
+  const STATUS_DRAFT=1;
+  const STATUS_PUBLISHED=2;
+  const STATUS_ARCHIVED=3;
+
 	/**
 	 * @return string the associated database table name
 	 */
@@ -30,33 +34,50 @@ class Post extends CActiveRecord
 	/**
 	 * @return array validation rules for model attributes.
 	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('title, content, status, author_id', 'required'),
-			array('status, create_time, update_time, author_id', 'numerical', 'integerOnly'=>true),
-			array('title', 'length', 'max'=>128),
-			array('tags', 'safe'),
-			// The following rule is used by search().
-			// @todo Please remove those attributes that should not be searched.
-			array('id, title, content, tags, status, create_time, update_time, author_id', 'safe', 'on'=>'search'),
-		);
-	}
+  public function rules()
+  {
+    return array(
+      array('title, content, status', 'required'),
+      array('title', 'length', 'max'=>128),
+      array('status', 'in', 'range'=>array(1,2,3)),
+      array('tags', 'match', 'pattern'=>'/^[\w\s,]+$/',
+          'message'=>'Tags can only contain word characters.'),
+      array('tags', 'normalizeTags'),
+
+      array('title, status', 'safe', 'on'=>'search'),
+    );
+  }
+
+  // Normalize tags
+  public function normalizeTags($attribute,$params)
+  {
+    $this->tags=Tag::array2string(array_unique(Tag::string2array($this->tags)));
+  }
+
+
+  // Adding url property
+  public function getUrl()
+  {
+    return Yii::app()->createUrl('post/view', array(
+        'id'=>$this->id,
+        'title'=>$this->title,
+    ));
+  }
 
 	/**
 	 * @return array relational rules.
 	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-			'author' => array(self::BELONGS_TO, 'User', 'author_id'),
-			'comments' => array(self::HAS_MANY, 'Comment', 'post_id'),
-		);
-	}
+  public function relations()
+  {
+    return array(
+      'author' => array(self::BELONGS_TO, 'User', 'author_id'),
+      'comments' => array(self::HAS_MANY, 'Comment', 'post_id',
+          'condition'=>'comments.status='.Comment::STATUS_APPROVED,
+          'order'=>'comments.create_time DESC'),
+      'commentCount' => array(self::STAT, 'Comment', 'post_id',
+          'condition'=>'status='.Comment::STATUS_APPROVED),
+    );
+  }
 
 	/**
 	 * @return array customized attribute labels (name=>label)
